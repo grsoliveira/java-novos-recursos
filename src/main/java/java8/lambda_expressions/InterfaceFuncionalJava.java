a92702d01ed58d23de9fa3bf8be04b0b12334ca0
Gleibson Oliveira <goliveira@olixcorp.com>
2023-01-03 23:37:22 -0300

    interface funcional java 8

---
+++ src/main/java/java8/lambda_expressions/InterfaceFuncionalJava.java
@@ -0,0 +1,26 @@
+package java8.lambda_expressions;
+
+public class InterfaceFuncionalJava {
+
+    public static void main(String[] args) {
+
+        /**
+         * Utilizando o modo tradicional do Java anterior ao 8
+         */
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Primeira thread em execução");
+            }
+        });
+        t1.start();
+
+        /**
+         * Utilizando a interface funcional definida pelo próprio Java
+         */
+        Thread t2 = new Thread( () -> System.out.println("Segunda thread em execução"));
+        t2.start();
+
+    }
+
+}
