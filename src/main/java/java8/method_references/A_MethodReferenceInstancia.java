9ec210a3bda8aa8b3ddbbc620ca23026b0b6d0d0
Gleibson Oliveira <goliveira@olixcorp.com>
2023-01-04 00:10:48 -0300

    methodo reference com método de instancia

---
+++ src/main/java/java8/method_references/A_MethodReferenceInstancia.java
@@ -0,0 +1,32 @@
+package java8.method_references;
+
+interface A_Figura2D{
+    void desenha(Double largura, Double altura);
+}
+
+class A_Retangulo {
+    public void desenhaRetangulo(Double largura, Double altura) {
+        System.out.println("Desenha retangulo largura: "+ largura + " e altura: " + altura);
+    }
+}
+
+public class A_MethodReferenceInstancia {
+
+    public static void main(String[] args) {
+        //utilizando lambda expression
+        A_Figura2D fig1 = (l, a) -> System.out.println("Desenha retangulo largura: "+ l + " e altura: " + a);
+        fig1.desenha(8.0, 1.5);
+
+        /**
+         * utilizando methodo reference
+         * classe/objeto :: método referenciado
+         * precisamos então de um objeto que tenha um metodo de desenho com 2 argumentos (semelhante ao da figura2D)
+         *
+         * o método desenha (de figura2d) será referenciado por um metodo de instancia de um objeto particular (ret)
+         * neste caso, o texto exibido será o conteúdo de desenhaRetangulo(...) devido à referência
+         */
+        A_Retangulo ret = new A_Retangulo();
+        A_Figura2D fig2 = ret::desenhaRetangulo;
+        fig2.desenha(10.5, 7.0);
+    }
+}
